793ece6c1776a7aead1a8b53e7017ff166c63c59
William Morton <91086457+WillTheWA@users.noreply.github.com>
2025-05-06 05:07:41 +0000

    Add files via upload

--- pomodoro.c
+++ pomodoro.c
@@ -2,12 +2,8 @@
 #include <stdint.h>
 #include "typeset.h"
 
-void main(void)
-{
-    // Load the tile set into VRAM
-    set_bkg_data(0, 44, Typeset);
-
-    // Create an array to fill the entire screen with the blank tile (index 44)
+void clear_screen() {
+	// Create an array to fill the entire screen with the blank tile (index 44)
     unsigned char tile_map[20 * 18];    // Screen size is 20x18 tiles
     for (int i = 0; i < 20 * 18; i++) {
         tile_map[i] = 44;               // Set every tile to the blank character (index 44)
@@ -15,18 +11,25 @@ void main(void)
 
     // Set the background with the blank character for the entire screen
     set_bkg_tiles(0, 0, 20, 18, tile_map);
+}
+
+void main(void) {
+    // Load the tile set into VRAM
+    set_bkg_data(0, 44, Typeset);
+    clear_screen();
 
     // Turn on the background and display
     SHOW_BKG;
     DISPLAY_ON;
 
     // Start game loop
-    unsigned char hello_map[] = {7, 4, 11, 11, 14};
-    unsigned char world_map[] = {22, 14, 17, 11, 3};
+    unsigned char pomodoro_map[] = {15, 14, 12, 14, 3, 14, 17, 14};
+    unsigned char timer_map[] = {19, 8, 12, 4, 17, 42};
     
     while(1) {
-        set_bkg_tiles(0, 0, 5, 1, hello_map);
-        set_bkg_tiles(0, 1, 5, 1, world_map);
+	// Write pomodoro timer
+        set_bkg_tiles(0, 0, 8, 1, pomodoro_map);
+        set_bkg_tiles(0, 1, 6, 1, timer_map);
         wait_vbl_done();
     }
 }
