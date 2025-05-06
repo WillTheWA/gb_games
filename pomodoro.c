df6936c8b5e33052a510838abd55e061edeb6f09
mortonwi <135153158+mortonwi@users.noreply.github.com>
2025-05-05 21:30:30 -0700

    Add files via upload

---
+++ pomodoro.c
@@ -0,0 +1,20 @@
+#include <gb/gb.h>
+#include <stdint.h>
+#include "typeset.h"
+
+void main(void)
+{
+    // Load letter set tiles
+    set_bkg_data(0, 44, Typeset);
+    //set_bkg_tiles(0, 0, 20, 18, Typeset); // x=2, y=10, 3 tiles wide
+
+    // Turn on background layer and display
+    // Start background
+    SHOW_BKG;
+    DISPLAY_ON;
+
+    // Start game loop
+    while(1) {
+        wait_vbl_done();
+    }
+}
