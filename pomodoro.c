050be5c804f42105a024d0f8412de6c0bf27c09f
mortonwi <135153158+mortonwi@users.noreply.github.com>
2025-05-07 12:50:14 -0700

    Update pomodoro.c

--- pomodoro.c
+++ pomodoro.c
@@ -47,11 +47,11 @@ void splash_screen() {
     unsigned char timer_map[] = {19, 8, 12, 4, 17, 42};
     unsigned char start_map[] = {18, 19, 0, 17, 19};
 
-    set_bkg_tiles(6, 4, 8, 1, pomodoro_map);  // Correct tile count here
+    set_bkg_tiles(6, 4, 8, 1, pomodoro_map);
     set_bkg_tiles(7, 5, 6, 1, timer_map);
     set_bkg_tiles(7, 10, 5, 1, start_map);
 
-    // Wait until start is pressed
+    // Wait until start button is pressed
     waitpad(J_START);
 }
 
@@ -69,8 +69,8 @@ void main(void) {
     clear_screen();  // Clear with blank tiles
     splash_screen();  // Show splash screen
     
-    // Load tiles
-    set_bkg_data(0, 227, background_data);  // Loading 227 background tiles
+    // Load 227 background tiles
+    set_bkg_data(0, 227, background_data);
     
     // Draw background map
     set_bkg_tiles(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT, background_map);
@@ -78,7 +78,7 @@ void main(void) {
     // Load Numset after background map
     set_bkg_data(227, 12, Numset);
 
-    // Timer start time
+    // Timer default start time and cycle coutn
     unsigned int minutes = 25;
     unsigned int seconds = 0;
     unsigned int cycle = 0;
@@ -96,13 +96,13 @@ void main(void) {
         set_bkg_tiles(TIMER_1X, TIMER_Y, 1, 1, &min_tiles[0]);
         set_bkg_tiles(TIMER_2X, TIMER_Y, 1, 1, &min_tiles[1]);
 
-        // Colon in between MM and SS (use tile 238, or whatever colon is)
-        set_bkg_tiles(14, TIMER_Y, 1, 1, (unsigned char[]){238});  // colon tile
+        // Colon in between MM and SS (tile 238)
+        set_bkg_tiles(14, TIMER_Y, 1, 1, (unsigned char[]){238});
 
         set_bkg_tiles(TIMER_3X, TIMER_Y, 1, 1, &sec_tiles[0]);
         set_bkg_tiles(TIMER_4X, TIMER_Y, 1, 1, &sec_tiles[1]);
 
-        // Delay ~1 second (this is a rough approximation)
+        // Delay ~1 second
         for (int i = 0; i < 60; i++) {
             wait_vbl_done();
         }
@@ -111,7 +111,12 @@ void main(void) {
         if (seconds == 0) {
             if (minutes == 0) {
                 // Timer done
+                // Increase cycle
                 cycle++;
+                
+                // 25 minute study, 5 minute rest for 3 cycles
+                //   then 25 minute study, 15 minute rest and 
+                //   reset cycle
                 if (cycle == 1 || cycle == 3 || cycle == 5) {
                     minutes = 5;
                 } else if (cycle == 7) {
@@ -119,6 +124,8 @@ void main(void) {
                     cycle = 0;
                 } else 
                     minutes = 25;
+                
+                // Wait until A button is pressed
                 waitpad(J_A);
             } else {
                 minutes--;
