9b79804524f930f8f40d79843db74f96448c824a
William Morton <91086457+WillTheWA@users.noreply.github.com>
2025-05-17 18:13:59 +0000

    Add files via upload

--- pomodoro.c
+++ pomodoro.c
@@ -30,6 +30,53 @@ unsigned char number_tiles[10] = {
     236  // 9
 };
 
+// Time delay function
+void pdelay(UINT8 numloops) {
+    UINT8 ii;
+    for(ii = 0; ii < numloops; ii++) {
+        wait_vbl_done();
+    }
+}
+
+// Screen fadeout
+void fadeout() {
+    for(int i = 0; i < 4; i++) {
+        switch(i) {
+	    case 0:
+	        BGP_REG = 0xE4;
+		break;
+            case 1:
+		BGP_REG = 0xF9;
+		break;
+	    case 2:
+		BGP_REG = 0xFE;
+		break;
+	    case 3:
+                BGP_REG = 0xFF;
+                break;		
+	}
+	pdelay(10);
+    }
+}
+
+// Screen fadein
+void fadein() {
+    for(int i = 0; i < 4; i++) {
+        switch(i) {
+	    case 0:
+	        BGP_REG = 0xFE;
+		break;
+            case 1:
+		BGP_REG = 0xF9;
+		break;
+	    case 2:
+		BGP_REG = 0xE4;
+		break;		
+	}
+	pdelay(10);
+    }
+}
+
 // Clear screen by filling it with blank tiles
 void clear_screen() {
     set_bkg_data(0, 44, Typeset);
@@ -47,12 +94,13 @@ void splash_screen() {
     unsigned char timer_map[] = {19, 8, 12, 4, 17, 42};
     unsigned char start_map[] = {18, 19, 0, 17, 19};
 
-    set_bkg_tiles(6, 4, 8, 1, pomodoro_map);
+    set_bkg_tiles(6, 4, 8, 1, pomodoro_map);  // Correct tile count here
     set_bkg_tiles(7, 5, 6, 1, timer_map);
     set_bkg_tiles(7, 10, 5, 1, start_map);
 
-    // Wait until start button is pressed
+    // Wait until start is pressed
     waitpad(J_START);
+    fadeout();
 }
 
 // Convert 2-digit number (like minutes or seconds) to 2 tiles
@@ -69,8 +117,8 @@ void main(void) {
     clear_screen();  // Clear with blank tiles
     splash_screen();  // Show splash screen
     
-    // Load 227 background tiles
-    set_bkg_data(0, 227, background_data);
+    // Load tiles
+    set_bkg_data(0, 227, background_data);  // Loading 227 background tiles
     
     // Draw background map
     set_bkg_tiles(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT, background_map);
@@ -78,7 +126,7 @@ void main(void) {
     // Load Numset after background map
     set_bkg_data(227, 12, Numset);
 
-    // Timer default start time and cycle coutn
+    // Timer start time
     unsigned int minutes = 25;
     unsigned int seconds = 0;
     unsigned int cycle = 0;
@@ -86,6 +134,8 @@ void main(void) {
     // Set min and sec tiles
     unsigned char min_tiles[2];
     unsigned char sec_tiles[2];
+
+    fadein();
 
     while (1) {
         // Convert minutes and seconds to tile data
@@ -96,13 +146,14 @@ void main(void) {
         set_bkg_tiles(TIMER_1X, TIMER_Y, 1, 1, &min_tiles[0]);
         set_bkg_tiles(TIMER_2X, TIMER_Y, 1, 1, &min_tiles[1]);
 
-        // Colon in between MM and SS (tile 238)
-        set_bkg_tiles(14, TIMER_Y, 1, 1, (unsigned char[]){238});
+        // Colon in between MM and SS (use tile 238, or whatever colon is)
+        unsigned char colon_map[] = {238};
+	set_bkg_tiles(14, TIMER_Y, 1, 1, colon_map);  // colon tile
 
         set_bkg_tiles(TIMER_3X, TIMER_Y, 1, 1, &sec_tiles[0]);
         set_bkg_tiles(TIMER_4X, TIMER_Y, 1, 1, &sec_tiles[1]);
 
-        // Delay ~1 second
+        // Delay ~1 second (this is a rough approximation)
         for (int i = 0; i < 60; i++) {
             wait_vbl_done();
         }
@@ -111,12 +162,7 @@ void main(void) {
         if (seconds == 0) {
             if (minutes == 0) {
                 // Timer done
-                // Increase cycle
                 cycle++;
-                
-                // 25 minute study, 5 minute rest for 3 cycles
-                //   then 25 minute study, 15 minute rest and 
-                //   reset cycle
                 if (cycle == 1 || cycle == 3 || cycle == 5) {
                     minutes = 5;
                 } else if (cycle == 7) {
@@ -124,8 +170,6 @@ void main(void) {
                     cycle = 0;
                 } else 
                     minutes = 25;
-                
-                // Wait until A button is pressed
                 waitpad(J_A);
             } else {
                 minutes--;
